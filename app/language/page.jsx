21ffeb720205dfce1f5c998f4d87baa50aa76330
Indrajit Yadav <indrajithyadav045@gmail.com>
2026-06-17 12:40:35 +0530

    Create page.jsx

---
+++ app/language/page.jsx
@@ -0,0 +1,15 @@
+export default function LanguagePage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Language Support</h1>
+      <p>Currently supported languages:</p>
+      <ul>
+        <li>English</li>
+        <li>Tamil</li>
+        <li>Hindi</li>
+        <li>Telugu</li>
+        <li>Malayalam</li>
+      </ul>
+    </main>
+  );
+}
