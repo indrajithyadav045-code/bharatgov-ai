bd3627d310aa51bcffb04ecc2ddbf2cd9fcf1654
Indrajit Yadav <indrajithyadav045@gmail.com>
2026-06-17 12:40:02 +0530

    Create page.jsx

---
+++ app/schemes/page.jsx
@@ -0,0 +1,14 @@
+export default function SchemesPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Government Schemes</h1>
+      <ul>
+        <li>National Scholarship Portal</li>
+        <li>PM Mudra Yojana</li>
+        <li>PM Kisan Samman Nidhi</li>
+        <li>Ayushman Bharat</li>
+        <li>PM Awas Yojana</li>
+      </ul>
+    </main>
+  );
+}
